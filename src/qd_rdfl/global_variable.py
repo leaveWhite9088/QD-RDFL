5dae6059901fa10dc0b711e7ff9d16d64f24c57f
leaveWhite9088 <1411169639@qq.com>
2025-04-05 15:33:15 +0800

    update global_variable

--- src/qd_rdfl/global_variable.py
+++ src/qd_rdfl/global_variable.py
@@ -1,13 +1,5 @@
 """全局变量"""
 
-"""================================= 路径参数 ================================="""
-
-# 可选：log-parameter_analysis,log-comparison,log-ablation,log-supplement
-
-global_minst_parent_path = "log-parameter_analysis"
-global_cifar10_parent_path = "log-parameter_analysis"
-global_cifar100_parent_path = "log-parameter_analysis"
-
 """================================= 超参数 ================================="""
 
 Lambda = 1
@@ -27,10 +19,20 @@ import argparse
 parser = argparse.ArgumentParser(description="Parameter Analysis for MNIST with XN.")
 
 # 添加命令行参数
-parser.add_argument('--adjustment_literation', type=float, default=-1, help="adjustment_literation")
+parser.add_argument('--adjustment_literation', type=float, default=0.01, help="adjustment_literation")
+parser.add_argument('--parent_path', type=str, default="log-main", help="parent_path")
 
 # 解析命令行参数
 args = parser.parse_args()
 
 # 使用命令行参数的值
 adjustment_literation = args.adjustment_literation
+parent_path = args.parent_path
+
+"""================================= 路径参数 ================================="""
+
+# 可选：log-parameter_analysis,log-comparison,log-ablation,log-supplement
+
+global_minst_parent_path = parent_path
+global_cifar10_parent_path = parent_path
+global_cifar100_parent_path = parent_path
